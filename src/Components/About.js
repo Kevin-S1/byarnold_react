8ba930c7ab79289fe1fd247f3a1f5cda8ed35535
Kevin Sips <kevin@Mac.local>
2022-04-14 16:35:48 +0200

    resolved conflict

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,6 @@ function About() {
       <section className="About">
         <img className='about-image about-image_left' src={require('../media/arnold2.jpg')}/>
         <article className="about-text">
-<<<<<<< HEAD
             <h2 className='about-header'>Over ByArnold:</h2>
             <p className='about-paragraph'>
             Na een lange periode in de logistieke sector te hebben gewerkt heb ik het roer omgegooid en ben ik mijn passie, het maken van industriële verlichting, achterna gegaan. <br></br><br></br>
@@ -16,17 +15,6 @@ function About() {
             <br></br><br></br>
             ByArnold staat voor eigentijds, rustiek en uniek!
             </p>
-=======
-          <h2 className='about-header'>Over ByArnold:</h2>
-          <p className='about-paragraph'>
-          Na een lange periode in de logistieke sector te hebben gewerkt heb ik het roer omgegooid en ben ik mijn passie, het maken van industriële verlichting, achterna gegaan. <br></br><br></br>
-          Geïnspireerd  door mijn vader, kunstenaar in roestvrijstaal, heb ik mij het lassen en omgaan met ijzer eigen gemaakt.
-          Mijn inspiratie haal ik uit industriële vormen. Doordat ik de verlichting zelf maak en bestaande voorwerpen vermaak ontstaat er een eigentijdse stijl waardoor elk item uniek is. Heeft u zelf een ontwerp dan is dat uiteraard bespreekbaar.. <br></br><br></br>
-          Mijn passie is het ontwerpen en maken van verlichting. Mijn lampen en meubels hebben een rustieke en industriele uitstraling en ik werk dan ook veel met staal, hout en hergebruikte materialen. Doordat ik de items zelf maak is het mogelijk om met een ontwerp te komen en dit in overleg bij mij te laten maken.
-          <br></br><br></br>
-          ByArnold staat voor eigentijds, rustiek en uniek!
-          </p>
->>>>>>> 0b72fa1dd218ea6e01bc6bd76f23e1444d63df5a
         </article>
         <img className='about-image about-image_right' src={require('../media/arnold2.jpg')}/>
       </section>
